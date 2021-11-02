b776d8551d4ee657ceee6cabaabaffab7ba57f90
sabiyoung <ethiodatecakes@gmail.com>
2021-11-02 11:36:13 -0700

    feat(component) add add-products & product-list

---
+++ src/client/src/app/pages/add-product/add-product.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { ProductService } from 'src/app/services/product.service';
+import { AppState } from 'src/app/store';
+import { createProducts } from 'src/app/store/actions/product/product.actions';
+import { Product } from './../../../../../shared/models/products.model';
+
+@Component({
+  selector: 'app-add-product',
+  templateUrl: './add-product.component.html',
+  styleUrls: ['./add-product.component.scss']
+})
+export class AddProductComponent implements OnInit {
+  addProduct: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppState>,
+    private productService: ProductService
+  ) {
+    this.addProduct = this.fb.group({
+
+     price: [
+        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      title: [
+        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+     image: [
+        '',  Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+     description: [
+        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+
+    })
+   }
+
+  ngOnInit(): void {
+  }
+addProducts() {
+  this.store.dispatch(createProducts({data: this.addProduct.value}))
+  console.log({data: this.addProduct.value})
+ this.addProduct.reset();
+}
+}
---
+++ src/client/src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store';
+import { loadProducts, selectProductAction } from './../../store/actions/product/product.actions';
+import { Product } from './../../../../../shared/models/products.model';
+import { Observable } from 'rxjs';
+import { productsSelector } from 'src/app/store/selectors/product/product.selectors';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  products$: Observable<Product[]>;
+
+  constructor(
+    private store: Store<AppState>,
+    private router: Router
+  ) {
+    this.products$ = this.store.select(productsSelector)
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(loadProducts())
+  }
+productDetail(selectedProduct: Product) {
+  this.store.dispatch(selectProductAction({data:selectedProduct}))
+  this.router.navigate(['/product-detail'])
+}
+
+
+}
