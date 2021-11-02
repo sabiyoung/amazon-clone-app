a7a0852015b39c82dd15c9f17f053ab5a0513bc2
sabiyoung <ethiodatecakes@gmail.com>
2021-11-02 11:37:24 -0700

    feat(component) add add-user & user-list

---
+++ src/client/src/app/pages/add-user/add-user.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store';
+import { createUser } from 'src/app/store/actions/user/user.actions';
+
+
+
+@Component({
+  selector: 'app-add-user',
+  templateUrl: './add-user.component.html',
+  styleUrls: ['./add-user.component.scss']
+})
+export class AddUserComponent implements OnInit {
+
+  addUser: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppState>,
+  ) {
+    this.addUser = this.fb.group({
+
+     name: [
+        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+     username: [
+        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+     email: [
+        '',  Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+    password: [
+        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+
+    })
+   }
+
+  ngOnInit(): void {
+  }
+addUsers() {
+  this.store.dispatch(createUser({data: this.addUser.value}))
+  console.log({data: this.addUser.value})
+this.addUser.reset();
+}
+}
