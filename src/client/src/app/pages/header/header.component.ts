309cc31f23ec9f0986faa96162f62a0299b9bce0
sabiyoung <ethiodatecakes@gmail.com>
2021-11-18 23:26:50 -0800

    feat(component) add signin and out

--- src/client/src/app/pages/header/header.component.ts
+++ src/client/src/app/pages/header/header.component.ts
@@ -3,9 +3,14 @@ import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AppState } from 'src/app/store';
-import { addToCartSelector } from 'src/app/store/selectors/product/product.selectors';
+import { addToCartSelector} from 'src/app/store/selectors/product/product.selectors';
 import { Product } from '../../../../../shared/models/products.model';
 import { ProductService } from 'src/app/services/product.service';
+import { User } from '../../../../../shared/models/user.model';
+import { loggedInUserSelector, usersSelector } from 'src/app/store/selectors/user/user.selectors';
+import { cartSelector } from 'src/app/store/selectors/cart/cart.selectors';
+import { Cart } from '../../../../../shared/models/cart.model';
+import { loginUser, logoutUser } from 'src/app/store/actions/user/user.actions';
 
 @Component({
   selector: 'app-header',
@@ -13,25 +18,33 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  cart$: Observable<Product[]>;
-  carts: Product []=[]
+  cart$: Observable<Cart | null>;
+  users$: Observable<User[]>
+  isAuthenticated$: Observable<User | null>;
 
   constructor(
     private store: Store<AppState>,
     private router: Router,
     private productService: ProductService
   ) {
-   this.cart$ = this.store.select(addToCartSelector)
+   this.cart$ = this.store.select(cartSelector)
+   this.users$ = this.store.select(usersSelector)
+   this.isAuthenticated$ = this.store.select(loggedInUserSelector);
   }
 
   ngOnInit(): void {
 
   }
-cart() {
+goToCart() {
   this.router.navigate(['/cart'])
 }
 home() {
   this.router.navigate(['/'])
 }
-
+signout() {
+  this.store.dispatch(logoutUser())
+}
+ordersPage() {
+this.router.navigate(['/orders'])
+}
 }
