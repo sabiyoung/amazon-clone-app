c4aad5f54ea8431301f4a9b6a73bc9530f1bf662
sabiyoung <ethiodatecakes@gmail.com>
2021-11-18 23:22:20 -0800

    feat(component) add login

---
+++ src/client/src/app/pages/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
+import { AppState } from 'src/app/store';
+import { loginUser, loginUserSuccess } from 'src/app/store/actions/user/user.actions';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  addUser: FormGroup;
+
+  constructor(private fb: FormBuilder,
+     private store: Store<AppState>,
+     private router: Router
+    ) {
+    this.addUser = this.fb.group({
+      email: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+    });
+  }
+
+  ngOnInit(): void {}
+
+login() {
+    this.store.dispatch(loginUser({ data: this.addUser.value }))
+ 
+  }
+signin() {
+  this.router.navigate(['/add-user'])
+}
+}
