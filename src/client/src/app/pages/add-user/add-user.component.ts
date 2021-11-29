a1a7c0410c0dd6a7f2d6b59efdf2f9c472323160
sabiyoung <ethiodatecakes@gmail.com>
2021-11-28 16:34:23 -0800

    ref(comp) used prettier

--- src/client/src/app/pages/add-user/add-user.component.ts
+++ src/client/src/app/pages/add-user/add-user.component.ts
@@ -4,43 +4,38 @@ import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store';
 import { createUser } from 'src/app/store/actions/user/user.actions';
 
-
-
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
-  styleUrls: ['./add-user.component.scss']
+  styleUrls: ['./add-user.component.scss'],
 })
 export class AddUserComponent implements OnInit {
-
   addUser: FormGroup;
-  constructor(
-    private fb: FormBuilder,
-    private store: Store<AppState>,
-  ) {
+  constructor(private fb: FormBuilder, private store: Store<AppState>) {
     this.addUser = this.fb.group({
-
-     name: [
-        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      name: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
       ],
-     username: [
-        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      username: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
       ],
-     email: [
-        '',  Validators.compose([Validators.required, Validators.minLength(3)]),
+      email: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
       ],
-    password: [
-        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      password: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
       ],
-
-    })
-   }
-
-  ngOnInit(): void {
+    });
   }
-addUsers() {
-  this.store.dispatch(createUser({data: this.addUser.value}))
-  console.log({data: this.addUser.value})
-this.addUser.reset();
-}
+
+  ngOnInit(): void {}
+  addUsers() {
+    this.store.dispatch(createUser({ data: this.addUser.value }));
+    console.log({ data: this.addUser.value });
+    this.addUser.reset();
+  }
 }
