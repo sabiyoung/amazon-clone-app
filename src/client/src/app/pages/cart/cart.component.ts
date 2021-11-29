a2fabae44adb5da7560b1ad63b8a58585d970ddb
sabiyoung <ethiodatecakes@gmail.com>
2021-11-28 16:23:47 -0800

    feat(comp) add load order action

--- src/client/src/app/pages/cart/cart.component.ts
+++ src/client/src/app/pages/cart/cart.component.ts
@@ -1,62 +1,63 @@
-import { Component, ElementRef, OnInit, ViewChild, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  OnChanges,
+  SimpleChange,
+  SimpleChanges,
+} from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Router } from '@angular/router';
 import { Observable, pipe } from 'rxjs';
 import { AppState } from 'src/app/store';
-import { addToCartSelector} from 'src/app/store/selectors/product/product.selectors';
+import { addToCartSelector } from 'src/app/store/selectors/product/product.selectors';
 import { Product } from '../../../../../shared/models/products.model';
 import { Cart } from '../../../../../shared/models/cart.model';
-import { decreaseQty,  getTotalSuccess, increaseQty, loadProducts } from 'src/app/store/actions/product/product.actions';
 import { ProductService } from 'src/app/services/product.service';
 import { cartSelector } from 'src/app/store/selectors/cart/cart.selectors';
-import {  deleteFromCart, loadCart, updateCart } from 'src/app/store/actions/cart/cart.actions';
-import { map, tap } from 'rxjs/operators';
+import {
+  deleteFromCart,
+  loadCart,
+  updateCart,
+} from 'src/app/store/actions/cart/cart.actions';
 import { updateReduceCart } from './../../store/actions/cart/cart.actions';
 import { CartService } from 'src/app/services/cart.service';
-
-
+import { loadOrder } from 'src/app/store/actions/order/order.actions';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
   products$: Observable<Product[]>;
-  products: []=[]
+  products: [] = [];
   cart$!: Observable<Cart | null>;
-
 
   constructor(
     private store: Store<AppState>,
     private router: Router,
-    private cartService: CartService,
-
+    private cartService: CartService
   ) {
-    this.products$ = this.store.select(addToCartSelector)
-   this.cart$ = this.store.select(cartSelector)
+    this.products$ = this.store.select(addToCartSelector);
+    this.cart$ = this.store.select(cartSelector);
   }
 
   ngOnInit(): void {
-  this.store.dispatch(loadCart())
-
+    this.store.dispatch(loadCart());
+    this.store.dispatch(loadOrder());
   }
 
+  onDecrement(product: Product) {
+    this.store.dispatch(updateReduceCart({ data: product }));
+  }
 
-onDecrement(product:Product) {
-  // this.cartService.updateCartReduce(product)
- this.store.dispatch(updateReduceCart({data:product}))
-}
-
-
-addToCart(product: Product) {
-  this.store.dispatch(updateCart({data:product}))
- }
+  addToCart(product: Product) {
+    this.store.dispatch(updateCart({ data: product }));
+  }
   deleteFromCart(product: Product) {
-    this.store.dispatch(deleteFromCart({data: product}))
-        console.log(`product '${product._id}' deleted successfully`);
-      }
-
-
-
+    this.store.dispatch(deleteFromCart({ data: product }));
+    console.log(`product '${product._id}' deleted successfully`);
+  }
 }
