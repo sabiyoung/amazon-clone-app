68a2c089f3ca7e0c000891de0cca4c831057c780
sabiyoung <ethiodatecakes@gmail.com>
2021-11-28 16:25:38 -0800

    feat(comp) add tax, shipping calculation and user adress form

--- src/client/src/app/pages/checkout/checkout.component.ts
+++ src/client/src/app/pages/checkout/checkout.component.ts
@@ -2,46 +2,100 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { AppState } from 'src/app/store';
-import { deleteFromCartSuccess } from 'src/app/store/actions/product/product.actions';
+import { cartSelector } from 'src/app/store/selectors/cart/cart.selectors';
 import { addToCartSelector } from 'src/app/store/selectors/product/product.selectors';
 import { usersSelector } from 'src/app/store/selectors/user/user.selectors';
+import { Cart } from '../../../../../shared/models/cart.model';
 import { Product } from '../../../../../shared/models/products.model';
 import { User } from '../../../../../shared/models/user.model';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { loadCart } from 'src/app/store/actions/cart/cart.actions';
+import { createAdress } from 'src/app/store/actions/user/user.actions';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
-  styleUrls: ['./checkout.component.scss']
+  styleUrls: ['./checkout.component.scss'],
 })
 export class CheckoutComponent implements OnInit {
-  products$: Observable<Product[]>;
-  users$:Observable<User[]>
-  products: Product[] = []
+  addCustomer: FormGroup;
+  cart$: Observable<Cart | null>;
   constructor(
     private store: Store<AppState>,
     private router: Router,
-
- 
+    private fb: FormBuilder
   ) {
-    this.products$ = this.store.select(addToCartSelector)
-    this.users$ = this.store.select(usersSelector)
+    this.cart$ = this.store.select(cartSelector);
+    this.addCustomer = this.fb.group({
+      firstName: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      lastName: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      adressLineOne: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(5)]),
+      ],
+      adressLineTwo: ['', Validators.compose([Validators.required])],
+      zipCode: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      state: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(2)]),
+      ],
+      city: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
+      ],
+      phoneNumber: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(10)]),
+      ],
+      email: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(4)]),
+      ],
+    });
   }
 
   ngOnInit(): void {
+    this.store.dispatch(loadCart());
   }
-  getAmount() {
-    let totalAmount = 0
-    this.products$.subscribe(products => this.products = products)
-    this.products.forEach(product => {
-      (product.price * product.quantity)
-  totalAmount += product.price
-    })
-    return totalAmount
+  getItems(product: Cart) {
+    return product.count! < 2 ? 'item' : 'items';
   }
-  deleteFromCart(product: Product) {
-    this.store.dispatch(deleteFromCartSuccess({data: product}))
-        console.log(`product '${product._id}' deleted successfully`);
-      }
+  addAdress() {
+    this.store.dispatch(createAdress({ data: this.addCustomer.value }));
+    console.log({ data: this.addCustomer.value });
+    this.addCustomer.reset();
+  }
+  getDate() {
+    const current = new Date();
+    const timestamp = current.getTime();
+  }
 
+  shipping(cart: Cart) {
+    return cart.total_amount! <= 25 ? cart.total_amount! / 10 : 'Free';
+  }
+
+  taxCalculation(cart: Cart) {
+    return cart.total_amount! / 14;
+  }
+  tatalBeforTax(cart: Cart) {
+    return cart.total_amount! <= 25
+      ? cart.total_amount! + cart.total_amount! / 10
+      : cart.total_amount;
+  }
+  tatalOrder(cart: Cart) {
+    return cart.total_amount! <= 25
+      ? cart.total_amount! + cart.total_amount! / 10 + cart.total_amount! / 14
+      : cart.total_amount! + cart.total_amount! / 14;
+  }
 }
