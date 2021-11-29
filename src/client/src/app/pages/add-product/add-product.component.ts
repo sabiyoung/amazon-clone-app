7e8c7883420e7a629bfa6e5df3caec4c9f2773d9
sabiyoung <ethiodatecakes@gmail.com>
2021-11-28 16:20:53 -0800

    ref(component) add div & disable atribute

--- src/client/src/app/pages/add-product/add-product.component.ts
+++ src/client/src/app/pages/add-product/add-product.component.ts
@@ -9,38 +9,40 @@ import { Product } from './../../../../../shared/models/products.model';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.scss']
+  styleUrls: ['./add-product.component.scss'],
 })
 export class AddProductComponent implements OnInit {
   addProduct: FormGroup;
+
   constructor(
     private fb: FormBuilder,
     private store: Store<AppState>,
     private productService: ProductService
   ) {
     this.addProduct = this.fb.group({
-
-     price: [
-        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      price: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
       ],
       title: [
-        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
       ],
-     image: [
-        '',  Validators.compose([Validators.required, Validators.minLength(3)]),
+      image: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
       ],
-     description: [
-        '', Validators.compose([Validators.required, Validators.minLength(3)]),
+      description: [
+        '',
+        Validators.compose([Validators.required, Validators.minLength(3)]),
       ],
-
-    })
-   }
-
-  ngOnInit(): void {
+    });
   }
-addProducts() {
-  this.store.dispatch(createProducts({data: this.addProduct.value}))
-  console.log({data: this.addProduct.value})
- this.addProduct.reset();
-}
+
+  ngOnInit(): void {}
+  addProducts() {
+    this.store.dispatch(createProducts({ data: this.addProduct.value }));
+    console.log({ data: this.addProduct.value });
+    this.addProduct.reset();
+  }
 }
