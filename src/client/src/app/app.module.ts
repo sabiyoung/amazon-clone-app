19a7e318e97438e3fdd99b634ba12a9faba4d166
sabiyoung <ethiodatecakes@gmail.com>
2021-11-28 16:19:10 -0800

    feat(component) install components

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -22,10 +22,26 @@ import {MatCardModule} from '@angular/material/card';
 import { CartComponent } from './pages/cart/cart.component';
 import { HeaderComponent } from './pages/header/header.component';
 import { AddUserComponent } from './pages/add-user/add-user.component';
-import { UserListComponent } from './pages/user-list/user-list.component';
 import { SubtotalComponent } from './pages/subtotal/subtotal.component';
 import { UserEffects } from './store/effects/user/user.effects';
 import * as fromUser from './store/reducers/user/user.reducer';
+import { LoginComponent } from './pages/login/login.component';
+import { DecimalPipe } from '@angular/common';
+
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { OrdersComponent } from './pages/orders/orders.component';
+import * as fromCart from './store/reducers/cart/cart.reducer';
+import { CartEffects } from './store/effects/cart/cart.effects';
+import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
+import * as fromOrder from './store/reducers/order/order.reducer';
+import { OrderEffects } from './store/effects/order/order.effects';
+import { RatingComponent } from './pages/rating/rating.component';
+import { RatingListComponent } from './pages/rating-list/rating-list.component';
+import { PaymentListComponent } from './pages/payment-list/payment-list.component';
+import {TextFieldModule} from '@angular/cdk/text-field';
+import { NgxStripeModule } from 'ngx-stripe';
+
+import { AdressListComponent } from './pages/adress-list/adress-list.component';
 
 @NgModule({
   declarations: [
@@ -38,25 +54,35 @@ import * as fromUser from './store/reducers/user/user.reducer';
     CartComponent,
     HeaderComponent,
     AddUserComponent,
-
-    UserListComponent,
      SubtotalComponent,
- 
+     LoginComponent,
+     CheckoutComponent,
+     OrdersComponent,
+     RatingComponent,
+     RatingListComponent,
+     PaymentListComponent,
+
+     AdressListComponent,
+
   ],
   imports: [
+    AngularFireFunctionsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     MatCardModule,
     MatIconModule,
-    EffectsModule.forRoot([ProductEffects, UserEffects]),
+    TextFieldModule,
+    NgxStripeModule.forRoot('pk_test_51ILWaRK6FJ4NvQJQAfitXtvoOesU79Mex29dVJK0y2CXbqoOdmeceakJ0NaCWwvKbUuKTtaSsOj4U2KQeBtfoNZA005UNamqdp'),
+    EffectsModule.forRoot([ProductEffects, UserEffects, CartEffects, OrderEffects]),
     FormsModule,
-
     ReactiveFormsModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     StoreModule.forFeature(fromProduct.productFeatureKey, fromProduct.reducer),
     StoreModule.forFeature(fromUser.userFeatureKey, fromUser.reducer),
+    StoreModule.forFeature(fromCart.cartFeatureKey, fromCart.reducer),
+    StoreModule.forFeature(fromOrder.orderFeatureKey, fromOrder.reducer),
 
   ],
   providers: [],
