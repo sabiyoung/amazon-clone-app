8419f18fe13b940495ecfc97f8b4c370669589ae
sabiyoung <ethiodatecakes@gmail.com>
2021-12-02 08:45:31 -0800

    ref(component) add user required

--- src/client/src/app/pages/add-user/add-user.component.ts
+++ src/client/src/app/pages/add-user/add-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store';
 import { createUser } from 'src/app/store/actions/user/user.actions';
@@ -11,23 +12,23 @@ import { createUser } from 'src/app/store/actions/user/user.actions';
 })
 export class AddUserComponent implements OnInit {
   addUser: FormGroup;
-  constructor(private fb: FormBuilder, private store: Store<AppState>) {
+  constructor(private fb: FormBuilder, private store: Store<AppState>, private router:Router) {
     this.addUser = this.fb.group({
       name: [
         '',
-        Validators.compose([Validators.required, Validators.minLength(3)]),
+        Validators.compose([Validators.required]),
       ],
       username: [
         '',
-        Validators.compose([Validators.required, Validators.minLength(3)]),
+        Validators.compose([Validators.required]),
       ],
       email: [
         '',
-        Validators.compose([Validators.required, Validators.minLength(3)]),
+        Validators.compose([Validators.required, Validators.email]),
       ],
       password: [
         '',
-        Validators.compose([Validators.required, Validators.minLength(3)]),
+        Validators.compose([Validators.required]),
       ],
     });
   }
@@ -38,4 +39,21 @@ export class AddUserComponent implements OnInit {
     console.log({ data: this.addUser.value });
     this.addUser.reset();
   }
+   // pattern('/^([a-zA-Z\d\.-]+)@([a-z\d-]+)\.([a-z{2,8}])(.\[a-z]{2,8})?$/')
+   
+get email() {
+  return this.addUser.get('email');
+} 
+get name() {
+  return this.addUser.get('name')
+} 
+get username() {
+  return this.addUser.get('username')
+} 
+get password () {
+  return this.addUser.get('password')
+} 
+login() {
+  this.router.navigate(['sign-in'])
+}
 }
