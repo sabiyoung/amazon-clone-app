5b6a519f7daaab938e10d665fb40a281809aef60
sabiyoung <ethiodatecakes@gmail.com>
2021-12-02 08:53:36 -0800

    save changes

--- dist/server/server.js
+++ dist/server/server.js
@@ -21,29 +21,12 @@ dotenv.config();
 const access_secret = process.env.ACCESS_TOKEN_SECRET;
 const PORT = process.env.PORT || 5000;
 const app = express();
-const publishable = process.env.PUBLISHABLE_KEY;
-const secret = process.env.SECRETE_KEY;
+const secret = process.env.STRIPE_SECRET_KEY;
 export const stripe = new Stripe(secret, {
     apiVersion: "2020-08-27",
 });
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.post("/create-payment", function (req, res) {
-    stripe.charges
-        .create({
-        amount: 7000,
-        description: "Web dev",
-        currency: "USD",
-        source: req.body.token,
-    })
-        .then((charge) => {
-        console.log(charge);
-        res.send("Success");
-    })
-        .catch((err) => {
-        res.send(err);
-    });
-});
 const saltRounds = 10;
 mongoose
     .connect(`${process.env.MONGO_URL}`)
@@ -61,6 +44,23 @@ app.use(cors({
     ],
 }));
 app.use(express.json());
+app.post("/api/create-payment", function (req, res) {
+    stripe.charges.create({
+        amount: req.body.amount,
+        description: "Payment",
+        currency: "USD",
+        source: req.body.id,
+    })
+        .then((charge) => {
+        console.log(charge);
+        res.json({ charge });
+    })
+        .catch((err) => {
+        console.log(err);
+        res.sendStatus(501);
+    });
+    console.log(req.body);
+});
 app.post("/api/create-product", function (req, res) {
     const { title, price, image, description, rating } = req.body;
     const product = new ProductModel({
--- src/client/src/environments/environment.prod.ts
+++ src/client/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+    STRIPE_PUBLISHABLE_KEY: "pk_test_51ILWaRK6FJ4NvQJQAfitXtvoOesU79Mex29dVJK0y2CXbqoOdmeceakJ0NaCWwvKbUuKTtaSsOj4U2KQeBtfoNZA005UNamqdp"
 };
+
